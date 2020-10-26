273a8128bec86d62f2d8b61c8ac0c349ca677c34
david-kimengu <davidmutune200@gmail.com>
2020-10-26 19:34:55 +0000

    Add function prototypes

--- holberton.h
+++ holberton.h
@@ -1,8 +1,11 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-
+int _strlen(const char *str);
+char *_strncpy(char *dest, char *src);
+char *_strrev (char *str);
+char * _itoa(int i, char *strout, int base);
+int _printf(const char *format, ...);
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-
 #endif
