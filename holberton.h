aae0a2fa7f3e52e93caec658c72e1d6590f64666
david-kimengu <davidmutune200@gmail.com>
2020-10-27 20:30:15 +0000

    Add stdio header

--- holberton.h
+++ holberton.h
@@ -5,8 +5,8 @@ char *_strncpy(char *dest, char *src);
 char *_strrev (char *str);
 char * _itoa(int i, char *strout, int base);
 int _printf(const char *format, ...);
-#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
 #endif
